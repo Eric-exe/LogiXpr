35c2c7f61e905d51a152ad918df11b954c3b476d
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-07-05 17:25:08 -0400

    Misc changes

--- include/parser.h
+++ include/parser.h
@@ -22,24 +22,23 @@ const static std::unordered_map<std::string, int> precedence = {
     {OR, 3},
     {XOR, 2},
     {IMPLIES, 1},
-    {IFF, 0}
-};
+    {IFF, 0}};
 
 /**
  * @brief Tokenize the expression
- * 
- * @param std::string string expression to be tokenized
- * 
+ *
+ * @param expression expression to be tokenized
+ *
  * @return string vector of tokens
  */
-std::vector<std::string> tokenize(std::string);
+std::vector<std::string> tokenize(std::string expression);
 
 /**
  * @brief Parse the expression and convert it to an abstract syntax tree
- * 
- * @param std::string string expression to be parsed
- * @param std::shared_ptr<Expression>& pointer to the root of the abstract syntax tree
- * 
+ *
+ * @param expression expression to be parsed
+ * @param root to the root of the abstract syntax tree
+ *
  * @return bool of whether the expression was parsed successfully
  */
-bool parse(std::string, std::shared_ptr<Expression>&);
+bool parse(std::string expression, std::shared_ptr<Expression> &root);
--- src/equivLaws.cpp
+++ src/equivLaws.cpp
@@ -5,8 +5,7 @@
 
 #include "../include/equivLaws.h"
 
-// (p & T) = p, (p | F) = p
-bool applyIdentity(std::shared_ptr<Expression> expression) {
+bool identity(std::shared_ptr<Expression> expression) {
     bool changed = false;
     if (expression->getValue() == AND) {
         if (expression->hasLeft() && expression->getLeft()->isVar() && expression->hasRight() && expression->getRight()->getValue() == TRUE) {
@@ -24,72 +23,3 @@ bool applyIdentity(std::shared_ptr<Expression> expression) {
     }
     return changed;
 }
-
-// (p | T) = T, (p & F) = F
-bool applyDomination(std::shared_ptr<Expression>); 
-
-// (p | p) = p, (p & p) = p
-bool applyIdempotence(std::shared_ptr<Expression>); 
-
-// !!p = p
-bool applyDoubleNegation(std::shared_ptr<Expression>);
-
-// p | q = q | p, p & q = q & p
-bool applyCommutative(std::shared_ptr<Expression>);
-
-// (p | q) | r = p | (q | r), (p & q) & r = p & (q & r)
-bool applyAssociative(std::shared_ptr<Expression>);
-
-// p | (q & r) = (p | q) & (p | r), p & (q | r) = (p & q) | (p & r)
-bool applyDistributive(std::shared_ptr<Expression>);
-
-// !(p | q) = !p & !q, !(p & q) = !p | !q
-bool applyDeMorgan(std::shared_ptr<Expression>);
-
-// p | (p & q) = p, p & (p | q) = p
-bool applyAbsorption(std::shared_ptr<Expression>);
-
-// p | !p = T, p & !p = F
-bool applyNegation(std::shared_ptr<Expression>);
-
-// p -> q = !p | q
-bool applyImplication0(std::shared_ptr<Expression>);
-
-// p -> q = !q -> !p
-bool applyImplication1(std::shared_ptr<Expression>);
-
-// p | q = !p -> q
-bool applyImplication2(std::shared_ptr<Expression>);
-
-// p & q = !(p -> !q)
-bool applyImplication3(std::shared_ptr<Expression>);
-
-// !(p -> q) = p & !q
-bool applyImplication4(std::shared_ptr<Expression>);
-
-// (p -> q) & (p -> r) = p -> (q & r)
-bool applyImplication5(std::shared_ptr<Expression>);
-
-// (p -> q) & (q -> r) = (p | q) -> r
-bool applyImplication6(std::shared_ptr<Expression>);
-
-// (p -> q) | (p -> r) = p -> (q | r)
-bool applyImplication7(std::shared_ptr<Expression>);
-
-// (p -> q) | (r -> q) = (p & r) -> q
-bool applyImplication8(std::shared_ptr<Expression>);
-
-// p <-> q = (p -> q) & (q -> p)
-bool applyBidirectionalImplication0(std::shared_ptr<Expression>);
-
-// p <-> q = q <-> p
-bool applyBidirectionalImplication1(std::shared_ptr<Expression>);
-
-// p <-> q = !p <-> !q
-bool applyBidirectionalImplication2(std::shared_ptr<Expression>);
-
-// p <-> q = (p & q) | (!p & !q)
-bool applyBidirectionalImplication3(std::shared_ptr<Expression>);
-
-// !(p <-> q) = p <-> !q
-bool applyBidirectionalImplication4(std::shared_ptr<Expression>);
